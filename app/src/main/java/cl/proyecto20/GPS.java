c315c3960333dfb92ba3bb60ebe9197f85ac1151
a201357160 <matiasgold18@gmail.com>
2023-11-18 13:49:29 -0300

    sabado 18

---
+++ app/src/main/java/cl/proyecto20/GPS.java
@@ -0,0 +1,126 @@
+package cl.proyecto20;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.health.connect.datatypes.ExerciseRoute;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import cl.proyecto20.databinding.ActivityGpsBinding;
+
+public class GPS extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private Marker marcador;
+    double lat = 0.0;
+    double lng = 0.0;
+    private ActivityGpsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityGpsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        miUbicacion();
+
+
+        // Add a marker in Sydney and move the camera
+
+        LatLng Konka = new LatLng(-36.60507644007202, -72.09145595149009);
+        LatLng Poizon = new LatLng(-36.605372315045976, -72.09886662678694);
+        LatLng Lugares1 = new LatLng(-36.613312017465695, -72.10626913126158);
+        LatLng Lugares2 = new LatLng(-36.61056509434619, -72.09995008196636);
+
+        mMap.addMarker(new MarkerOptions().position(Konka).title("Konka Grow Shop"));
+        mMap.addMarker(new MarkerOptions().position(Poizon).title("Poizon Grow Shop"));
+        mMap.addMarker(new MarkerOptions().position(Lugares1).title("Konka Grow Shop-Puren"));
+        mMap.addMarker(new MarkerOptions().position(Lugares2).title("A PLANTAR GROWSHOP"));
+
+        String ubicacion1 = getIntent().getStringExtra("Ubicacion1");
+        String[] partes1 = ubicacion1.split(",");
+        String latitud1 = partes1[0];
+        String longitud1 = partes1[1];
+        double lt1 = Double.parseDouble(latitud1);
+        double lg1 = Double.parseDouble(longitud1);
+
+        LatLng ubi1 = new LatLng(lt1, lg1);
+        mMap.addMarker(new MarkerOptions()
+                .position(ubi1)
+                .title("Ubicacion 1"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubi1, 10));
+
+
+    }
+
+    private void agregarMarcador(double lat, double lng) {
+        LatLng coordenadas = new LatLng(lat, lng);
+        CameraUpdate miUbicacion = CameraUpdateFactory.newLatLngZoom(coordenadas, 16);
+        if (marcador != null) marcador.remove();
+        marcador = mMap.addMarker(new MarkerOptions().position(coordenadas).title("Mi posicion Actual").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.animateCamera(miUbicacion);
+    }
+
+    private void actualizarUbicacion(Location location) {
+        if (location != null) {
+            lat = location.getLatitude();
+            lng = location.getLongitude();
+            agregarMarcador(lat, lng);
+        }
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            actualizarUbicacion(location);
+        }
+    };
+
+    private void miUbicacion() {
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        actualizarUbicacion(location);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,15000,0,locationListener);
+    }
+}
