542330b5a7aa035497937ac0354305a6fa5acd49
Evaluacion02 <frankbailey440@gmail.com>
2023-12-05 14:46:01 -0300

    version

--- app/src/main/java/cl/proyecto20/ingresodatos.java
+++ app/src/main/java/cl/proyecto20/ingresodatos.java
@@ -5,42 +5,57 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ingresodatos extends AppCompatActivity {
 
-    private EditText Ubicacion1;
+    EditText texto, nombre, apellido;
+    Button btnEnviar, btnmap;
 
-    private Button agregarButon;
-    private Button ingresoBoton;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ingresodatos);
-        Intent intent = new Intent(this,GPS.class);
-        ingresoBoton = (Button) findViewById(R.id.Btingreso);
-        ingresoBoton.setOnClickListener(new View.OnClickListener() {
+        Intent intent = new Intent(this, ingresodatos.class);
+        btnmap = (Button) findViewById(R.id.btnGPS);
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        nombre = findViewById(R.id.txtNombre);
+        apellido = findViewById(R.id.txtApellido);
+        btnEnviar = findViewById(R.id.btnIngresar);
+
+
+        inicializarFireBase();
+
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                databaseReference.child("Mensaje").setValue(texto.getText().toString());
+                databaseReference.child("Nombre").setValue(nombre.getText().toString());
+                databaseReference.child("Apellido").setValue(apellido.getText().toString());
+                Toast.makeText(getApplicationContext(), "Mensaje enviado exitosamente", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Consulta tu servidor MQTT y la base de datos de Firebase", Toast.LENGTH_LONG).show();
+
+            }
+        });
+        btnmap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){startActivity(intent); }
         });
-
-        Ubicacion1 = findViewById(R.id.D1);
-        agregarButon = findViewById(R.id.BtAgregar);
-
-        agregarButon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String ubi1 = Ubicacion1.getText().toString();
-
-
-                Intent intent = new Intent(ingresodatos.this, GPS.class);
-
-                intent.putExtra("Ubicacion1", ubi1);
-                startActivity(intent);
-            }
-        });
     }
 
+    private void inicializarFireBase(){
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
 }
